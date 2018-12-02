444e0285ee62cd0bf0b3a4cd7f42bcf0fa9d7c1d
felipe <fcamparasilva@gmail.com>
2018-12-02 12:44:35 -0300

    Feat(Connection) Create Database Connection and Test

--- app/src/main/java/com/school/org/school/MainActivity.java
+++ app/src/main/java/com/school/org/school/MainActivity.java
@@ -1,14 +1,24 @@
 package com.school.org.school;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import database.DataOpenHelper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SQLiteDatabase connection;
+    private DataOpenHelper dataOpenHelper;
+    private LinearLayout layoutContentMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +28,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.mViewHolder.buttonGradle = (Button) findViewById(R.id.btnGrades);
         this.mViewHolder.buttonStudent = (Button) findViewById(R.id.btnStudent);
         this.mViewHolder.buttonTeacher = (Button) findViewById(R.id.btnTeacher);
+        layoutContentMain = (LinearLayout) findViewById(R.id.layoutMain);
 
         this.mViewHolder.buttonGradle.setOnClickListener(this);
         this.mViewHolder.buttonStudent.setOnClickListener(this);
         this.mViewHolder.buttonTeacher.setOnClickListener(this);
+
+        createConnection();
 
     }
 
@@ -44,6 +57,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intentTeacher);
                 break;
         }
+    }
+
+    private void createConnection () {
+
+        try {
+
+            dataOpenHelper = new DataOpenHelper(this);
+            connection = dataOpenHelper.getWritableDatabase();
+            Snackbar.make(layoutContentMain, "Conexão criado com sucesso", Snackbar.LENGTH_SHORT)
+                    .setAction("Ok!", null).show();
+
+        } catch(SQLException ex) {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle("Erro");
+            dlg.setMessage(ex.getMessage());
+            dlg.setNeutralButton("Ok", null);
+            dlg.show();
+        }
+
     }
 
     private static class ViewHolder {
--- app/src/main/java/database/ScriptDLL.java
+++ app/src/main/java/database/ScriptDLL.java
@@ -5,7 +5,7 @@ public class ScriptDLL {
     public static String getCreateTableStudent () {
         StringBuilder sql = new StringBuilder();
 
-        sql.append("CREATE TABLE IF NOT EXIST STUDENT (");
+        sql.append("CREATE TABLE IF NOT EXISTS STUDENT (");
         sql.append("        ID_STUDENT INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ");
         sql.append("        NAME VARCHAR(250) NOT NULL DEFAULT ('') )");
 
