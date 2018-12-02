f84892866071fd132861052df0bde86aac3141bf
felipe <fcamparasilva@gmail.com>
2018-12-02 10:11:16 -0300

    Feat(Create Views) Finish structures to views

---
+++ app/src/main/java/com/school/org/school/GradleActivity.java
@@ -0,0 +1,13 @@
+package com.school.org.school;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GradleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gradle);
+    }
+}
